d8869640a0fe3313c84ab4523c4802f2c0bb4140
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-10-08 16:14:29 +0200

    Script async rendering

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ const App: React.FC = () => {
             type="checkbox"
             checked={displayJokes}
             onChange={() => setDisplayJokes(!displayJokes)}
+            autoFocus
           />
           Display jokes
         </label>
